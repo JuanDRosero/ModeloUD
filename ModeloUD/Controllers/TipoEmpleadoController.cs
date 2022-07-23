ce6f22c85e94e22391c368bbadc32e2068fcecd9
Juan David Rosero Torres <jdrosero59@gmail.com>
2022-07-23 18:29:48 -0500

    Creación controladores y carpetas

---
+++ ModeloUD/Controllers/TipoEmpleadoController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ModeloUD.Controllers
+{
+    public class TipoEmpleadoController : Controller
+    {
+        private readonly ILogger<SedeController> _logger;
+        public TipoEmpleadoController(ILogger<SedeController> logger)
+        {
+            this._logger = logger;
+        }
+        // GET: TipoEmpleadoController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: TipoEmpleadoController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: TipoEmpleadoController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TipoEmpleadoController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TipoEmpleadoController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: TipoEmpleadoController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TipoEmpleadoController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: TipoEmpleadoController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
